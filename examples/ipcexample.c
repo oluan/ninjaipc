8ce5feaa3f703313ef831e223df3d01dfd17de78
Julimar Melo <melobrdev@gmail.com>
2022-03-08 18:10:03 -0300

    (core-view): created impl to nj create view interface on linux #12

--- examples/ipcexample.c
+++ examples/ipcexample.c
@@ -2,7 +2,7 @@
 #include "../src/ninjall.h"
 #include "../src/ninjasync.h"
 #include "../src/ninjaview.h"
-#include <Windows.h>
+// #include <Windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,47 +11,50 @@ void fn(const char *buf) { printf("[RECEIVED] %s\n", buf); }
 void fn2(const char *buf) { printf("[RECEIVED2] %s\n", buf); }
 void fn3(const char *buf) { printf("[RECEIVED3] %s\n", buf); }
 
-DWORD thread(LPVOID lparam) {
-	ninjasync *obj = (ninjasync*)lparam;
-	printf("Received: %p\n", obj->obj_handle);
-	Sleep(5000);
-	printf("Notifying...\n");
-	nj_notify_sync_obj(obj);
-	return 0;
-}
+// DWORD thread(LPVOID lparam) {
+// 	ninjasync *obj = (ninjasync*)lparam;
+// 	printf("Received: %p\n", obj->obj_handle);
+// 	Sleep(5000);
+// 	printf("Notifying...\n");
+// 	nj_notify_sync_obj(obj);
+// 	return 0;
+// }
 
 int main() {
-  ninjall_node *head = ll_init();
-  ll_register_callback(head, fn);
-  ll_register_callback(head, fn2);
-  ll_register_callback(head, fn3);
-  ll_notify_all_callbacks(head, ":)");
+  // ninjall_node *head = ll_init();
+  // ll_register_callback(head, fn);
+  // ll_register_callback(head, fn2);
+  // ll_register_callback(head, fn3);
+  // ll_notify_all_callbacks(head, ":)");
 
-  ninjasync obj = nj_create_sync_obj("nj_ipc_sync");
+  // ninjasync obj = nj_create_sync_obj("nj_ipc_sync");
 
-  if (obj.status) {
-	  printf("Created\n");
-  }
+  // if (obj.status) {
+  //  printf("Created\n");
+  // }
 
-  nj_bool st = nj_wait_notify_sync_obj_timed(&obj, 2000);
+  // nj_bool st = nj_wait_notify_sync_obj_timed(&obj, 2000);
 
-  printf("Timeout ? should be 0: (%d)\n", st);
+  // printf("Timeout ? should be 0: (%d)\n", st);
 
-  CreateThread(0, 0, thread, (LPVOID)&obj, 0, 0);
+  // CreateThread(0, 0, thread, (LPVOID)&obj, 0, 0);
 
-  nj_wait_notify_sync_obj(&obj);
-  
-  printf("Notified!\n");
+  // nj_wait_notify_sync_obj(&obj);
+  //
+  // printf("Notified!\n");
 
-  CloseHandle(obj.obj_handle);
+  // CloseHandle(obj.obj_handle);
 
   ninjaview v = nj_create_view("nj_ipc_view", 1024);
 
-  nj_write_to_view(&v, "Melo48", strlen("Melo48"));
-  nj_write_to_view(&v, "Me", strlen("Me"));
-  nj_write_to_view(&v, "Mel", strlen("Mel"));
+  if (v.status == nj_true) {
+    printf("Sucessfully view created!\n");
+  }
+  // nj_write_to_view(&v, "Melo48", strlen("Melo48"));
+  // nj_write_to_view(&v, "Me", strlen("Me"));
+  // nj_write_to_view(&v, "Mel", strlen("Mel"));
 
-  printf("View: %d!\nBuffer: %s\n", v.status, (char*)v.view_buffer);
+  printf("View: %d!\nBuffer: %s\n", v.status, (char *)v.view_buffer);
 
-  CloseHandle(v.view_fd);
+  //  CloseHandle(v.view_fd);
 }
