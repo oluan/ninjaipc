afc2177834d9283c67334941e85e483c561c54ba
Luan Devecchi <luan@engineer.com>
2022-08-18 23:25:18 -0300

    (style): update style

--- src/ninjaview.h
+++ src/ninjaview.h
@@ -1,10 +1,7 @@
 /**
  * This file is part of the "ninjaipc" project.
  *
- * Copyright (c) 2022
- *
- * Luan Devecchi <luan@engineer.com>
- * Julimar Melo <melobrdev@gmail.com>
+ * Copyright (c) 2022, ninjaipc authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +17,7 @@
 #include <memory.h>
 
 typedef struct ninjaview {
-  void* view_fd;
+  void *view_fd;
   void *view_buffer;
   unsigned int view_size;
   nj_bool status;
@@ -30,19 +27,21 @@ ninjaview nj_create_view(const char *view_name, unsigned int view_size);
 
 inline nj_bool nj_write_to_view(ninjaview *view_obj, void *blob,
                                 unsigned int blob_size) {
-  // Clear buffer
+  /* Clear buffer */
   if (memset(view_obj->view_buffer, '\0', view_obj->view_size)) {
-    // If blob not invalid
+    /* If blob not invalid */
     if (blob) {
-      // Copies the blob to the view buffer and returns nj_true
-      // if succesfully copied
+      /*
+       * Copies the blob to the view buffer and returns nj_true
+       * if succesfully copied
+       */
       if (memcpy(view_obj->view_buffer, blob, blob_size)) {
         return nj_true;
       }
     }
-    // Blob invalid
+    /* Blob invalid */
     return nj_false;
   }
-  // Memset failed
+  /* Memset failed */
   return nj_false;
 }
