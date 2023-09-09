7bc853bc4bdd8b946476c8a4b717df62268f12f8
Luan Devecchi <luan@engineer.com>
2023-09-09 10:14:03 -0300

    chore: make an useful header

--- src/ninjaipc.h
+++ src/ninjaipc.h
@@ -1,16 +1,16 @@
-/**
- * This file is part of the "ninjaipc" project.
- *
- * Copyright (c) 2023, ninjaipc authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ * ninjaipc: A cross-platform header-only library for Inter-Process Communication (IPC)
+ * 
+ * Features:
+ * - Synchronization API: Allows for inter-process signaling using synchronization objects.
+ * - Shared Memory API: Allows for sharing memory between processes for data interchange.
+ * - Callback Storage API: Provides a way to store and execute callback functions.
+ * 
+ * The library currently supports Windows. POSIX support is planned for future releases.
+ * 
+ * Please see the individual API functions for more details.
+ * 
+ * Naming of APIs are namespaced, the format is always: nj_ipc_${API}_${FUNCTION}
  */
 
 #ifndef _NINJAIPC_H
